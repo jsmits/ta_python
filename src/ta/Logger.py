16735cfeb6d2b3fb9b52dadd6d2cb0a27fd135ff
jsmits <jsmits@7961b8e5-0c0d-0410-93b5-baf7301955fc>
2006-06-04 17:54:51 +0000

    git-svn-id: https://svn.huttt.com/TA_python@87 7961b8e5-0c0d-0410-93b5-baf7301955fc

--- src/ta/Logger.py
+++ src/ta/Logger.py
@@ -10,16 +10,18 @@ import sys
 logger_format = '%(asctime)s  %(levelname)-9.9s %(message)s  %(module)s.py:%(lineno)d'
 logger_date_format = '%d-%m-%Y %H:%M:%S'
 logger_level = int(os.environ.get('TA_LOGLEVEL', logging.INFO))
-logger_handlers = [(logging.StreamHandler(), logging.INFO), 
-           (logging.handlers.TimedRotatingFileHandler(filename='ta.log', when='midnight', backupCount=10), logging.DEBUG)]
+logger_level_debug = int(os.environ.get('TA_DEBUG_LOGLEVEL', logging.DEBUG))
+logger_handlers = [(logging.StreamHandler(), logger_level), 
+           (logging.handlers.RotatingFileHandler(filename='ta.log', maxBytes=10*1024*1024, backupCount=10), logger_level_debug)]
     
-def logger(name='ta', level=logger_level, format=logger_format,
+def logger(name='ta', level=logger_level_debug, format=logger_format,
            date_format=logger_date_format):
     """ logger(level) -> returns a logger all fixed up
         add handlers if needed in the logger_handlers list
     
     """
     logger = logging.getLogger(name)
+    logger.setLevel(level)
     formatter = logging.Formatter(format, date_format)
     for handler, handler_level in logger_handlers:
         handler.setLevel(handler_level)
