3299d9c33d7e9f6f1f1572c81b5d6dc76b07eb60
jsmits <jsmits@7961b8e5-0c0d-0410-93b5-baf7301955fc>
2006-06-04 16:22:56 +0000

    git-svn-id: https://svn.huttt.com/TA_python@80 7961b8e5-0c0d-0410-93b5-baf7301955fc

---
+++ src/ta/Logger.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+""" ta.Logger -> Logging module for use elsewhere in this package.
+
+"""
+import logging
+import logging.handlers
+import os
+import sys
+
+logger_format = '%(asctime)s  %(levelname)-9.9s %(message)s  %(module)s.py:%(lineno)d'
+logger_date_format = '%d-%m-%Y %H:%M:%S'
+logger_level = int(os.environ.get('TA_LOGLEVEL', logging.INFO))
+
+def logger(name='ta', level=logger_level, format=logger_format,
+           date_format=logger_date_format):
+    """ logger(level) -> returns a logger all fixed up
+        add handlers if needed in the logger_handlers list
+    
+    """
+    logger_handlers = [(logging.StreamHandler(), logging.INFO), 
+           (logging.handlers.TimedRotatingFileHandler(filename=name+'.log', when='midnight', backupCount=10), logging.DEBUG)]
+    
+    logger = logging.getLogger(name)
+    formatter = logging.Formatter(format, date_format)
+    for handler, handler_level in logger_handlers:
+        handler.setLevel(handler_level)
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+    return logger
+    
+
