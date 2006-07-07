eec6d5f411573f9eecad2bf900e11394b67c7d5c
jsmits <jsmits@7961b8e5-0c0d-0410-93b5-baf7301955fc>
2006-07-07 20:05:44 +0000

    git-svn-id: https://svn.huttt.com/TA_python@100 7961b8e5-0c0d-0410-93b5-baf7301955fc

--- src/ta/Indicator.py
+++ src/ta/Indicator.py
@@ -1,21 +1,42 @@
 import datetime
 
+# define exceptions
+class IndicatorError(Exception): pass
+class NotTupleError(IndicatorError): pass
+class NotFloatError(IndicatorError): pass
+class InvalidCandleStickError(IndicatorError): pass
+class InvalidDateTimeError(IndicatorError): pass
+
+#signal
+class Signal:
+    def __init__(self, indicator):
+        self.indicator = indicator
+
 class Indicator:
-    def __init__(self):
+    def __init__(self, parameter, *args, **kwargs):
+        self.parameter = parameter
+        self.row = 4 # close
+        if args: self.row = args[0]
+        
         self.times = []
         self.opens = []
         self.highs = []
         self.lows = []
         self.closes = []
         self.volumes = []
+         
+    def append(self, candle):
+        # check if valid input
+        validateInput(candle, self.times)
+        # check for virtual candle
+        if len(self.times) > 0 and self.times[-1] == candle[0]:
+            self.revertToPreviousState()
+        self.calculate(candle)
+        self.updateLists(candle)
+        self.signals()
         
-        self.input = []
-        self.output = []
-        
-        self.row = 4 # close
-        
-    def handleVirtualCandle(self, value):
-        # revert back to previous state, remove previous virtual candle
+    def revertToPreviousState(self):
+        # remove previous virtual candle
         self.times = self.times[:-1]
         self.opens = self.opens[:-1]
         self.highs = self.highs[:-1]
@@ -24,51 +45,40 @@ class Indicator:
         self.volumes = self.volumes[:-1]
         self.input = self.input[:-1]
         self.output = self.output[:-1]
-         
-    def append(self, candle):
-        # check if valid input
-        validateInput(candle, self.times)
-        # check for virtual candle
-        if len(self.times) > 0 and self.times[-1] == candle[0]:
-            self.handleVirtualCandle(candle)
-        if self.calculate(candle):
-            self.updateLists(candle)
-            self.signals()
             
-    def calculate(self, value):
+    def calculate(self, candle):
         pass
     
-    def updateLists(self, value):
-        self.times.append(value[0])   # datetime
-        self.opens.append(value[1])   # open
-        self.highs.append(value[2])   # high
-        self.lows.append(value[3])    # low
-        self.closes.append(value[4])  # close
-        self.volumes.append(value[5]) # volume
+    def updateLists(self, candle):
+        self.times.append(candle[0])   # datetime
+        self.opens.append(candle[1])   # open
+        self.highs.append(candle[2])   # high
+        self.lows.append(candle[3])    # low
+        self.closes.append(candle[4])  # close
+        self.volumes.append(candle[5]) # volume
         
     def signals(self):
         pass
-        
-    
-        
-# define exceptions
-class IndicatorError(Exception): pass
-class NotTupleError(IndicatorError): pass
-class NotFloatError(IndicatorError): pass
-class InvalidCandleStickError(IndicatorError): pass
-class InvalidDateTimeError(IndicatorError): pass
 
-def validateInput(value, times):
-    if type(value) is not tuple:
-        raise NotTupleError, 'invalid input: should be a tuple (d, o, h, l, c, v); input: %s' % (value,)
-    if len(value) != 6:
-        raise InvalidCandleStickError, 'invalid input: tuple length should be 6; input: %s' % (value,)
-    if not type(value[0]) is datetime.datetime:
-        raise InvalidDateTimeError, 'invalid input: tuple element [0] should be a datetime; input: %s' % (value[0],)
+def validateInput(candle, times):
+    if type(candle) is not tuple:
+        raise NotTupleError, 'invalid input: should be a tuple (d, o, h, l, c, v); input: %s' % (candle, )
+    if len(candle) < 6:
+        raise InvalidCandleStickError, 'invalid input: tuple length should be 6 or more; input: %s' % (candle, )
+    if not type(candle[0]) is datetime.datetime:
+        raise InvalidDateTimeError, 'invalid input: tuple element [0] should be a datetime; input: %s' % (candle[0], )
     for i in range(1,5):
-        if type(value[i]) is not int and type(value[i]) is not float:
-            raise InvalidCandleStickError, 'invalid input: tuple element [%s] is not int or float; input: %s' % (i,value[i])
-    if len(times) > 0 and value[0] < times[-1]:
-        raise InvalidDateTimeError, 'invalid input: tuple element [0] (datetime) should be equal or greater than previous: %s; input: %s' % (times[-1], value[0])  
-    if type(value[5]) is not int:
-        raise InvalidCandleStickError, 'invalid input: volume should be int; input: %s' % value[5]
+        if type(candle[i]) is not int and type(candle[i]) is not float:
+            raise InvalidCandleStickError, 'invalid input: tuple element [%s] is not int or float; input: %s' % (i, candle[i])
+        elif candle[i] <= 0:
+            raise InvalidCandleStickError, 'invalid input: tuple element [%s] is equal or less than 0; input: %s' % (i, candle[i])
+    if candle[2] < candle[3]:
+        raise InvalidCandleStickError, 'invalid input: high: %s is lower than low: %s' % (candle[2], candle[3])
+    if candle[1] > candle[2] or candle[1] < candle[3]:
+        raise InvalidCandleStickError, 'invalid input: open (%s) is outside high (%s) - low (%s) range' % (candle[1], candle[2], candle[3])
+    if candle[4] > candle[2] or candle[4] < candle[3]:
+        raise InvalidCandleStickError, 'invalid input: close (%s) is outside high (%s) - low (%s) range' % (candle[4], candle[2], candle[3])
+    if len(times) > 0 and candle[0] < times[-1]:
+        raise InvalidDateTimeError, 'invalid input: tuple element [0] (datetime) should be equal or greater than previous: %s; input: %s' % (times[-1], candle[0])  
+    if type(candle[5]) is not int:
+        raise InvalidCandleStickError, 'invalid input: volume should be int; input: %s' % candle[5]
