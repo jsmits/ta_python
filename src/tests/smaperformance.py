095ff06e65a6ad6b99d9893237277f7b0067a69e
jsmits <jsmits@7961b8e5-0c0d-0410-93b5-baf7301955fc>
2006-07-07 20:05:21 +0000

    git-svn-id: https://svn.huttt.com/TA_python@98 7961b8e5-0c0d-0410-93b5-baf7301955fc

--- src/tests/smaperformance.py
+++ src/tests/smaperformance.py
@@ -2,7 +2,7 @@ from ta.Sma import *
 import datetime
 import random
 
-ind = Sma(3)
+ind = Sma(13)
 
 d = datetime.datetime(2005, 1, 1, 9, 30)
 td = datetime.timedelta(0, 1)
@@ -10,13 +10,16 @@ close = 12.20
 nrofticks = int(60 * 60 * 6.5) # one market day with one tick every second
 start = datetime.datetime.now()
 for x in xrange(nrofticks):
-    c = (d, 12.34, 12.56, 12.11, close, 20192812)
-    ind.append(c)
+    c = (d, close+0.01, close+0.04, close-0.10, close, 20192812)
+    try:
+        ind.append(c)
+    except Signal, obj:
+        #print "Signal from: %s" % obj.indicator
+        pass
     d = d + td
     if (random.random() > 0.4): close = close + random.random()/2
     else: close = close - random.random()/2
 end = datetime.datetime.now()
 diff = end - start
-logger.debug(ind)
 dfsec = float("" + str(diff.seconds) + "." + str(diff.microseconds))
-logger.info("Inserting %s candles took %s seconds. %s candles per second." % (nrofticks, dfsec, nrofticks / dfsec))
+print "Inserting %s candles took %s seconds. %s candles per second." % (nrofticks, dfsec, nrofticks / dfsec)
