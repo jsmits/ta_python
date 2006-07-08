352804d18adedb90882b19b12591c67a9e3f1fd1
jsmits <jsmits@7961b8e5-0c0d-0410-93b5-baf7301955fc>
2006-07-08 10:23:55 +0000

    git-svn-id: https://svn.huttt.com/TA_python@107 7961b8e5-0c0d-0410-93b5-baf7301955fc

--- src/ta/Macd.py
+++ src/ta/Macd.py
@@ -1,63 +1,56 @@
-import logging
-import logging.config
-from Ema import Ema
+from Indicator import *
+from Ema import Ema, EmaMacd
 
-class Macd:
+class Macd(Indicator):
     """ Moving Average Convergence-Divergence (MACD) indicator class
             input:      data        = list or tuple of float or integer values
                         parameter   = tuple (x, y, z); where x is short term ema, y is long term ema, and z is ema of the macd"""
   
-    def __init__(self, parameter, *args):
-        self.parameter = parameter
-        self.row = 4 # close
-        if args: self.row = args[0]
+    def __init__(self, parameter, *args, **kwargs):
+        Indicator.__init__(self, parameter, *args, **kwargs)
         self.input = []
-        self.s_ema = Ema(parameter[0])
-        self.l_ema = Ema(parameter[1])
+        self.s_ema = Ema(parameter[0], self.row)
+        self.l_ema = Ema(parameter[1], self.row)
         self.output = [] # macd
-        self.ema_macd = Ema(parameter[2])
+        self.ema_macd = EmaMacd(parameter[2])
         
-        logging.config.fileConfig("../logging.conf")
-        self.logger = logging.getLogger("Indicator")
-        
-    def append(self, value):
-        # check if valid input
-        if not self._validate(value): return
-        if type(value) is tuple: value = value[self.row]
-        if self._calculate(value):
-            return self.output[-1]
-        
-    def _validate(self, value):
-        if not type(value) is float and not type(value) is int and not type(value) is tuple:
-            self.logger.debug('invalid input: %s; should be either float or int or tuple' % value)
-            return False
-        else: return True
-        
-    def _calculate(self, value):
+    def calculate(self, candle):
+        value = candle[self.row]
         self.input.append(float(value))
-        self.s_ema.append(float(value))
-        self.l_ema.append(float(value))
+        try: self.s_ema.append(candle)
+        except Signal: pass
+        try: self.l_ema.append(candle)
+        except Signal: pass
         
         outputvalue = None
-        ema_macd = None
         if len(self.input) >= self.parameter[1]:
             try:    
                 outputvalue = self.s_ema[-1] - self.l_ema[-1]
             except:
-                self.logger.debug('error calculating macd value; reverting input data back to previous state')
                 self.input = self.input[:-1]
                 self.s_ema = self.s_ema[:-1]
                 self.l_ema = self.l_ema[:-1]
-                return False
+                raise IndicatorError, 'error calculating macd value; reverting input data back to previous state'
         self.output.append(outputvalue)
-        self.ema_macd.append(outputvalue)
-        return True
+        ema_macd_candle = None
+        # make fake candle for ema_macd
+        if outputvalue != None: ema_macd_candle = (candle[0], outputvalue, outputvalue, outputvalue, outputvalue, 0)
+        self.ema_macd.append(ema_macd_candle)
+        
+    def revertToPreviousState(self):
+        # remove previous virtual candle
+        Indicator.revertToPreviousState(self)
+        self.input = self.input[:-1]
+        self.output = self.output[:-1]
+        self.s_ema.revertToPreviousState()
+        self.l_ema.revertToPreviousState()
+        self.ema_macd.revertToPreviousState()
     
     # overloads
     def __str__(self):
         string = ''
         for i in xrange(len(self.input)):
-            string+='%s\t%s\t%s\t%s\t%s\t%s\n' % (i+1, self.input[i], str(self.s_ema[i])[:7], str(self.l_ema[i])[:7], str(self.output[i])[:7], str(self.ema_macd[i]))
+            string+='%s\t%s\t%s\t%s\t%s\t%s\t%s\n' % (i+1, self.times[i], self.input[i], str(self.s_ema[i])[:7], str(self.l_ema[i])[:7], str(self.output[i])[:7], str(self.ema_macd[i]))
         return 'Macd(%s):\n%s' % (self.parameter, string)
     def __repr__(self):
         return 'Macd(%s)' % self.parameter
@@ -80,6 +73,7 @@ if __name__=='__main__':
              (datetime.datetime(2006, 5, 8), 12.34, 12.56, 12.11, 12.20, 2010912),
              (datetime.datetime(2006, 5, 9), 12.24, 12.48, 12.20, 12.22, 8791029),
              (datetime.datetime(2006, 5, 10), 12.18, 12.20, 11.88, 12.16, 5434255),
+             (datetime.datetime(2006, 5, 10), 12.18, 13.22, 11.88, 13.22, 8434255),
              (datetime.datetime(2006, 5, 11), 12.24, 12.68, 12.24, 12.38, 8734251),
              (datetime.datetime(2006, 5, 12), 12.30, 12.88, 12.28, 12.57, 3637262),
              (datetime.datetime(2006, 5, 15), 12.34, 12.56, 12.11, 12.20, 2010912),
