8be8854065ad2b37acfb74997571ba80f0512ef7
jsmits <jsmits@7961b8e5-0c0d-0410-93b5-baf7301955fc>
2006-07-09 11:06:15 +0000

    Measures Macd performance.
    git-svn-id: https://svn.huttt.com/TA_python@130 7961b8e5-0c0d-0410-93b5-baf7301955fc

---
+++ src/tests/macdperformance.py
@@ -0,0 +1,24 @@
+from ta.Macd import *
+import datetime
+import random
+
+ind = Macd((12,26,9))
+
+d = datetime.datetime(2005, 1, 1, 9, 30)
+td = datetime.timedelta(0, 1)
+close = 12.20
+nrofticks = int(60 * 60 * 6.5) # one market day with one tick every second
+start = datetime.datetime.now()
+for x in xrange(nrofticks):
+    c = (d, close+0.01, close+0.04, close-0.10, close, 20192812)
+    try:
+        ind.append(c)
+    except Signal, obj:
+        pass
+    d = d + td
+    if (random.random() > 0.4): close = close + random.random()/2
+    else: close = close - random.random()/2
+end = datetime.datetime.now()
+diff = end - start
+dfsec = float("" + str(diff.seconds) + "." + str(diff.microseconds))
+print "Inserting %s candles in Macd indicator took %s seconds. %s candles per second." % (nrofticks, dfsec, nrofticks / dfsec)
