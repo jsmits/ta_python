2318b564cce1714115e387c07b17e2f98efa419e
jsmits <jsmits@7961b8e5-0c0d-0410-93b5-baf7301955fc>
2006-07-09 11:05:28 +0000

    git-svn-id: https://svn.huttt.com/TA_python@128 7961b8e5-0c0d-0410-93b5-baf7301955fc

--- src/tests/emamacdtest.py
+++ src/tests/emamacdtest.py
@@ -12,23 +12,21 @@ from ta.Ema import *
 import unittest
 
 inputValues = [(datetime.datetime(2006, 5, 1), 12.34, 12.56, 12.11, 12.20, 2010912),
-             (datetime.datetime(2006, 5, 2), 12.24, 12.48, 12.20, 12.22, 8791029),
+             (datetime.datetime(2006, 5, 2), 12.24, 12.48, 12.20, 12.20, 8791029),
+                (datetime.datetime(2006, 5, 2), 12.24, 12.48, 12.20, 12.21, 8791029),
+                (datetime.datetime(2006, 5, 2), 12.24, 12.48, 12.20, 12.22, 8791029),
              (datetime.datetime(2006, 5, 3), 12.18, 12.20, 11.88, 12.16, 5434255),
-             (datetime.datetime(2006, 5, 4), 12.24, 12.68, 12.24, 12.38, 8734251),
+             (datetime.datetime(2006, 5, 4), 12.24, 12.68, 12.24, 12.36, 8734251),
+                (datetime.datetime(2006, 5, 4), 12.34, 12.78, 12.20, 12.38, 6534253),
              (datetime.datetime(2006, 5, 5), 12.30, 12.88, 12.28, 12.57, 3637262),
              (datetime.datetime(2006, 5, 8), 12.34, 12.56, 12.11, 12.20, 2010912),
              (datetime.datetime(2006, 5, 9), 12.24, 12.48, 12.20, 12.22, 8791029),
              (datetime.datetime(2006, 5, 10), 12.18, 12.20, 11.88, 12.16, 5434255),
-                  (datetime.datetime(2006, 5, 10), 12.18, 12.20, 11.88, 12.16, 5434255),
-                  (datetime.datetime(2006, 5, 10), 12.18, 12.20, 11.88, 12.16, 5434255),
-                  (datetime.datetime(2006, 5, 10), 12.18, 12.20, 11.88, 12.16, 5434255),
-                  (datetime.datetime(2006, 5, 10), 12.18, 12.20, 11.88, 12.16, 5434255),
              (datetime.datetime(2006, 5, 11), 12.24, 12.68, 12.24, 12.38, 8734251),
              (datetime.datetime(2006, 5, 12), 12.30, 12.88, 12.28, 12.57, 3637262),
              (datetime.datetime(2006, 5, 15), 12.34, 12.56, 12.11, 12.20, 2010912),
              (datetime.datetime(2006, 5, 16), 12.24, 12.48, 12.20, 12.22, 8791029),
              (datetime.datetime(2006, 5, 17), 12.18, 12.20, 11.88, 12.16, 5434255),
-                  (datetime.datetime(2006, 5, 17), 12.18, 12.20, 11.88, 12.16, 5434255),
              (datetime.datetime(2006, 5, 18), 12.24, 12.68, 12.24, 12.38, 8734251),
              (datetime.datetime(2006, 5, 19), 12.30, 12.88, 12.28, 12.57, 3637262),
           ]
@@ -142,7 +140,7 @@ class KnownValues(unittest.TestCase):
         """
         s = EmaMacd(4)
         knownvalues = [None, None, None, 12.24, 12.372, 12.3032, 12.26992, 12.225952]
-        for c in inputValues[:8]:
+        for c in inputValues[:11]:
             try:
                 s.append(c)
             except Signal, obj:
