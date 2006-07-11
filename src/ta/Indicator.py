a48cee6f5f2608015614e13a0eefae2215e1a6c7
jsmits <jsmits@7961b8e5-0c0d-0410-93b5-baf7301955fc>
2006-07-11 18:21:11 +0000

    git-svn-id: https://svn.huttt.com/TA_python@142 7961b8e5-0c0d-0410-93b5-baf7301955fc

--- src/ta/Indicator.py
+++ src/ta/Indicator.py
@@ -7,8 +7,8 @@ class NotTupleError(InvalidCandleStickError): pass
 class NotFloatError(InvalidCandleStickError): pass
 class InvalidDateTimeError(InvalidCandleStickError): pass
 
-#signal
-class Signal:
+#status
+class Status:
     def __init__(self, indicator):
         self.indicator = indicator
 
@@ -28,13 +28,12 @@ class Indicator:
          
     def append(self, candle):
         # check if valid input
-        validateInput(candle, self.times)
+        self.validateInput(candle)
         # check for virtual candle
         if len(self.times) > 0 and self.times[-1] == candle[0]:
             self.revertToPreviousState()
         self.calculate(candle)
         self.updateLists(candle)
-        self.signals()
         
     def revertToPreviousState(self):
         # remove previous virtual candle
@@ -59,28 +58,25 @@ class Indicator:
         self.closes.append(candle[4])  # close
         self.volumes.append(candle[5]) # volume
         
-    def signals(self):
-        pass
-
-def validateInput(candle, times):
-    if type(candle) is not tuple:
-        raise NotTupleError, 'invalid input: should be a tuple (d, o, h, l, c, v); input: %s' % (candle, )
-    if len(candle) < 6:
-        raise InvalidCandleStickError, 'invalid input: tuple length should be 6 or more; input: %s' % (candle, )
-    if not type(candle[0]) is datetime.datetime:
-        raise InvalidDateTimeError, 'invalid input: tuple element [0] should be a datetime; input: %s' % (candle[0], )
-    for i in range(1,5):
-        if type(candle[i]) is not int and type(candle[i]) is not float:
-            raise InvalidCandleStickError, 'invalid input: tuple element [%s] is not int or float; input: %s' % (i, candle[i])
-        elif candle[i] <= 0:
-            raise InvalidCandleStickError, 'invalid input: tuple element [%s] is equal or less than 0; input: %s' % (i, candle[i])
-    if candle[2] < candle[3]:
-        raise InvalidCandleStickError, 'invalid input: high: %s is lower than low: %s' % (candle[2], candle[3])
-    if candle[1] > candle[2] or candle[1] < candle[3]:
-        raise InvalidCandleStickError, 'invalid input: open (%s) is outside high (%s) - low (%s) range' % (candle[1], candle[2], candle[3])
-    if candle[4] > candle[2] or candle[4] < candle[3]:
-        raise InvalidCandleStickError, 'invalid input: close (%s) is outside high (%s) - low (%s) range' % (candle[4], candle[2], candle[3])
-    if len(times) > 0 and times[-1] != None and candle[0] < times[-1]:
-        raise InvalidDateTimeError, 'invalid input: tuple element [0] (datetime) should be equal or greater than previous: %s; input: %s' % (times[-1], candle[0])  
-    if type(candle[5]) is not int:
-        raise InvalidCandleStickError, 'invalid input: volume should be int; input: %s' % candle[5]
+    def validateInput(self, candle):
+        if type(candle) is not tuple:
+            raise NotTupleError, 'invalid input: should be a tuple (d, o, h, l, c, v); input: %s' % (candle, )
+        if len(candle) < 6:
+            raise InvalidCandleStickError, 'invalid input: tuple length should be 6 or more; input: %s' % (candle, )
+        if not type(candle[0]) is datetime.datetime:
+            raise InvalidDateTimeError, 'invalid input: tuple element [0] should be a datetime; input: %s' % (candle[0], )
+        for i in range(1,5):
+            if type(candle[i]) is not int and type(candle[i]) is not float:
+                raise InvalidCandleStickError, 'invalid input: tuple element [%s] is not int or float; input: %s' % (i, candle[i])
+            elif candle[i] <= 0:
+                raise InvalidCandleStickError, 'invalid input: tuple element [%s] is equal or less than 0; input: %s' % (i, candle[i])
+        if candle[2] < candle[3]:
+            raise InvalidCandleStickError, 'invalid input: high: %s is lower than low: %s' % (candle[2], candle[3])
+        if candle[1] > candle[2] or candle[1] < candle[3]:
+            raise InvalidCandleStickError, 'invalid input: open (%s) is outside high (%s) - low (%s) range' % (candle[1], candle[2], candle[3])
+        if candle[4] > candle[2] or candle[4] < candle[3]:
+            raise InvalidCandleStickError, 'invalid input: close (%s) is outside high (%s) - low (%s) range' % (candle[4], candle[2], candle[3])
+        if len(self.times) > 0 and self.times[-1] != None and candle[0] < self.times[-1]:
+            raise InvalidDateTimeError, 'invalid input: tuple element [0] (datetime) should be equal or greater than previous: %s; input: %s' % (self.times[-1], candle[0])  
+        if type(candle[5]) is not int:
+            raise InvalidCandleStickError, 'invalid input: volume should be int; input: %s' % candle[5]
